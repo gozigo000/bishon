21f4d41121836a3497d8fa18d355be5371b9af5f
gozigo000 <143670404+gozigo000@users.noreply.github.com>
2025-05-14 12:55:27 +0900

    [elemExtractor] 부호의 설명 추출 및 테스트 코드 추가

---
+++ src/kipo/ooxmlToHlz/elemExtractor.ts
@@ -0,0 +1,46 @@
+import { dlog } from "@/_utils/env";
+
+/**
+ * 명세서에서 발명의 구성요소를 추출하는 함수
+ * @param text 명세서 텍스트
+ * @returns 추출된 구성요소 배열
+ */
+export function extractElements(text: string): string[] {
+
+    const matches = Array.from(text.matchAll(/([^().,]+)\((\d+\w?)\)/g));
+    if (!matches.length) return [];
+
+    const elemBag: Record<string, string> = {}
+    matches.forEach(match => {
+        const [_, text, num] = match;
+        if (!elemBag[num]) {
+            elemBag[num] = text.trim();
+        }
+        else if (elemBag[num] !== text) {
+            const suffix = findCommonSuffix(elemBag[num], text);
+            elemBag[num] = suffix.trim();
+        }
+    });
+
+    function findCommonSuffix(s1: string, s2: string): string {
+        let i = s1.length - 1;
+        let j = s2.length - 1;
+        let common = '';
+        while (i >= 0 && j >= 0 && s1[i] === s2[j]) {
+            common = s1[i] + common;
+            i--; j--;
+        }
+        return common;
+    }
+
+    const orderedElems = Object.entries(elemBag)
+        .sort(([numA], [numB]) => {
+            const a = numA.replace(/\d/g, '');
+            const b = numB.replace(/\d/g, '');
+            return a.localeCompare(b);
+        })
+        .sort(([numA], [numB]) => parseInt(numA) - parseInt(numB))
+        .map(([num, text]) => `${text}(${num})`);
+
+    return orderedElems;
+}
