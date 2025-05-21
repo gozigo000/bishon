12a6571203a1932638591f1c97aec410c6faf4a6
gozigo000 <143670404+gozigo000@users.noreply.github.com>
2025-05-21 18:25:26 +0900

    전각 스페이스 제거 취소함함

--- src/kipo/kipoInspection/kipoInspector.ts
+++ src/kipo/kipoInspection/kipoInspector.ts
@@ -95,7 +95,7 @@ class KipoInspector {
         // inspect_bracket - E201, E204, E211 에러 수정
         this.roughXml = this.roughXml
             .replaceAll('【', '[').replaceAll('】', ']')
-            .replaceAll('　', ' '); // 全角 스페이스 제거
+            // .replaceAll('　', ' '); // 全角 스페이스 제거
 
         const headTags = this.roughXml.match(/^[\s\S]*documentID="\d{10}">/)?.[0];
         if (!headTags) return this.report.record({ 
