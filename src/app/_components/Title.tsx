063278eb320a4d8ff889d76605ee6eef1bfb5f29
gozigo000 <143670404+gozigo000@users.noreply.github.com>
2025-06-26 18:14:44 +0900

    버전 수정 (v2.0)

--- src/app/_components/Title.tsx
+++ src/app/_components/Title.tsx
@@ -3,7 +3,7 @@ export default function Title() {
     return (
         <div className="flex flex-col w-full h-26 pt-10 pb-10">
             <h2 className="text-center text-2xl sm:text-3xl md:text-4xl font-bold text-white">
-                HLZ/FIN 만들기
+                HLZ/FIN 만들기 v2.0
             </h2>
         </div>
     );
